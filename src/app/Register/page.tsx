f7290f02c8556ca91e6f2d8bfe07ec31143bf6e8
Abdullah <abdullah@prodoo.com>
2025-09-30 12:19:52 +0500

    images and ui fixes

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -62,61 +62,62 @@ export default function Register() {
         <Card className="overflow-hidden">
           <CardContent className="grid p-0 md:grid-cols-2">
             <FormProvider {...methods}>
-              <form
-                className="p-6 md:p-8 min-h-[550px] max-h-max relative"
-                onSubmit={methods.handleSubmit(onSubmit)}
-              >
-                <Button
-                  variant={"link"}
-                  type="button"
-                  onClick={() => router.push("/")}
-                  className="mb-4 text-fixnix-darkpurple hover:underline text-sm absolute top-2 left-2"
+              <div className="max-sm:p-2 sm:p-6 md:p-8">
+                <form
+                  className=" min-h-[550px] max-h-max relative"
+                  onSubmit={methods.handleSubmit(onSubmit)}
                 >
-                  ← Back
-                </Button>
-                <div className="flex flex-col gap-6">
-                  <div className="flex flex-col items-center text-center">
-                    <h1 className="text-2xl font-bold">Register</h1>
-                  </div>
-                  <div className="grid gap-2">
-                    <FormInput label="Full Name" type="text" name="name" />
-                  </div>
-                  <div className="grid gap-2">
-                    <FormInput label="Email" type="email" name="email" />
-                  </div>
-                  <div className="grid gap-2">
-                    <FormInput
-                      label="Password"
-                      type="password"
-                      name="password"
-                    />
-                    <div className="flex items-center">
-                      <Link
-                        href="/forget-password"
-                        className="ml-auto text-sm underline-offset-2 hover:underline"
-                      >
-                        Forgot your password?
-                      </Link>
-                    </div>
-                  </div>
                   <Button
-                    disabled={loading}
-                    type="submit"
-                    className="w-full bg-fixnix-lightpurple"
+                    variant={"link"}
+                    type="button"
+                    onClick={() => router.push("/")}
+                    className="mb-4 text-fixnix-darkpurple hover:underline text-sm absolute top-2 left-2"
                   >
-                    {loading ? (
-                      <LoaderCircle className="animate-spin h-5 w-5" />
-                    ) : (
-                      "Register"
-                    )}
+                    ← Back
                   </Button>
-                  {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                  <div className="flex flex-col gap-6">
+                    <div className="flex flex-col items-center text-center">
+                      <h1 className="text-2xl font-bold">Register</h1>
+                    </div>
+                    <div className="grid gap-2">
+                      <FormInput label="Full Name" type="text" name="name" />
+                    </div>
+                    <div className="grid gap-2">
+                      <FormInput label="Email" type="email" name="email" />
+                    </div>
+                    <div className="grid gap-2">
+                      <FormInput
+                        label="Password"
+                        type="password"
+                        name="password"
+                      />
+                      <div className="flex items-center">
+                        <Link
+                          href="/forget-password"
+                          className="ml-auto text-sm underline-offset-2 hover:underline"
+                        >
+                          Forgot your password?
+                        </Link>
+                      </div>
+                    </div>
+                    <Button
+                      disabled={loading}
+                      type="submit"
+                      className="w-full bg-fixnix-lightpurple"
+                    >
+                      {loading ? (
+                        <LoaderCircle className="animate-spin h-5 w-5" />
+                      ) : (
+                        "Register"
+                      )}
+                    </Button>
+                    {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
                       Or continue with
                     </span>
                   </div> */}
-                  <div className="grid grid-cols-3 gap-4 w-full">
-                    {/* <Button variant="outline" className="w-full">
+                    <div className="grid grid-cols-3 gap-4 w-full">
+                      {/* <Button variant="outline" className="w-full">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24"
@@ -128,22 +129,23 @@ export default function Register() {
                           </svg>
                           <span className="sr-only">Login with Google</span>
                         </Button> */}
-                    <div
-                      id="google-btn"
-                      className="flex justify-center col-span-3"
-                    ></div>
+                      <div
+                        id="google-btn"
+                        className="flex justify-center col-span-3"
+                      ></div>
+                    </div>
+                    <div className="text-center text-sm">
+                      Already have an account?{" "}
+                      <Link
+                        href="/login"
+                        className="underline underline-offset-4"
+                      >
+                        Login
+                      </Link>
+                    </div>
                   </div>
-                  <div className="text-center text-sm">
-                    Already have an account?{" "}
-                    <Link
-                      href="/login"
-                      className="underline underline-offset-4"
-                    >
-                      Login
-                    </Link>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </FormProvider>
 
             <div className="relative hidden bg-muted md:block">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -156,66 +156,67 @@ export default function Login() {
   };
 
   return (
-    <div className="max-w-6xl w-full mx-auto py-10 px-4">
+    <div className="max-w-6xl w-full mx-auto py-10 sm:px-4 px-2">
       <div className={cn("flex flex-col gap-6")}>
         <Card className="overflow-hidden">
           <CardContent className="grid p-0 md:grid-cols-2">
             <FormProvider {...methods}>
-              <form
-                className="p-6 md:p-8 min-h-[550px] max-h-max relative"
-                onSubmit={methods.handleSubmit(onSubmit)}
-              >
-                <Button
-                  variant={"link"}
-                  type="button"
-                  onClick={() => router.push("/")}
-                  className="mb-4 text-fixnix-darkpurple hover:underline text-sm absolute top-2 left-2"
+              <div className="max-sm:p-2 sm:p-6 md:p-8">
+                <form
+                  className=" min-h-[550px] max-h-max relative"
+                  onSubmit={methods.handleSubmit(onSubmit)}
                 >
-                  ← Back
-                </Button>
-                <div className="flex flex-col gap-6">
-                  <div className="flex flex-col items-center text-center">
-                    <h1 className="text-2xl font-bold">Welcome back</h1>
-                    <p className="text-balance text-muted-foreground">
-                      Login to SSC
-                    </p>
-                  </div>
-                  <div className="grid gap-2">
-                    <FormInput label="Email" type="email" name="email" />
-                  </div>
-                  <div className="grid gap-2">
-                    <FormInput
-                      label="Password"
-                      type="password"
-                      name="password"
-                    />
-                    <div className="flex items-center">
-                      <Link
-                        href="/forget-password"
-                        className="ml-auto text-sm underline-offset-2 hover:underline"
-                      >
-                        Forgot your password?
-                      </Link>
-                    </div>
-                  </div>
                   <Button
-                    disabled={loading}
-                    type="submit"
-                    className="w-full bg-fixnix-lightpurple"
+                    variant={"link"}
+                    type="button"
+                    onClick={() => router.push("/")}
+                    className="mb-4 text-fixnix-darkpurple hover:underline text-sm absolute top-2 left-2"
                   >
-                    {loading ? (
-                      <LoaderCircle className="animate-spin h-5 w-5" />
-                    ) : (
-                      "Login"
-                    )}
+                    ← Back
                   </Button>
-                  <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                      Or continue with
-                    </span>
-                  </div>
-                  <div className="grid grid-cols-3 gap-4 w-full">
-                    {/* <Button variant="outline" className="w-full">
+                  <div className="flex flex-col gap-6">
+                    <div className="flex flex-col items-center text-center">
+                      <h1 className="text-2xl font-bold">Welcome back</h1>
+                      <p className="text-balance text-muted-foreground">
+                        Login to SSC
+                      </p>
+                    </div>
+                    <div className="grid gap-2">
+                      <FormInput label="Email" type="email" name="email" />
+                    </div>
+                    <div className="grid gap-2">
+                      <FormInput
+                        label="Password"
+                        type="password"
+                        name="password"
+                      />
+                      <div className="flex items-center">
+                        <Link
+                          href="/forget-password"
+                          className="ml-auto text-sm underline-offset-2 hover:underline"
+                        >
+                          Forgot your password?
+                        </Link>
+                      </div>
+                    </div>
+                    <Button
+                      disabled={loading}
+                      type="submit"
+                      className="w-full bg-fixnix-lightpurple"
+                    >
+                      {loading ? (
+                        <LoaderCircle className="animate-spin h-5 w-5" />
+                      ) : (
+                        "Login"
+                      )}
+                    </Button>
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4 w-full">
+                      {/* <Button variant="outline" className="w-full">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
@@ -227,22 +228,23 @@ export default function Login() {
                       </svg>
                       <span className="sr-only">Login with Google</span>
                     </Button> */}
-                    <div
-                      id="google-btn"
-                      className="flex justify-center col-span-3"
-                    ></div>
+                      <div
+                        id="google-btn"
+                        className="flex justify-center col-span-3"
+                      ></div>
+                    </div>
+                    <div className="text-center text-sm">
+                      Don&apos;t have an account?{" "}
+                      <Link
+                        href="/Register"
+                        className="underline underline-offset-4"
+                      >
+                        Sign up
+                      </Link>
+                    </div>
                   </div>
-                  <div className="text-center text-sm">
-                    Don&apos;t have an account?{" "}
-                    <Link
-                      href="/Register"
-                      className="underline underline-offset-4"
-                    >
-                      Sign up
-                    </Link>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </FormProvider>
 
             <div className="relative hidden bg-muted md:block">
